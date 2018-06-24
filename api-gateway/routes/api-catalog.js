c4eb21c31d3402386a4b8c68ab64084afd572598
Jared Brown <brownjt01@sunyjcc.edu>
2018-06-24 16:32:20 -0400

    Adding Week 8 Exercise

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -1,11 +1,12 @@
 var express = require('express'); var router = express.Router();
 var auth_controller = require('../controllers/authController');
 var bodyParser = require('body-parser')
+var checkToken = require("../check-token");
 // POST request for registering a user 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 router.post('/auth/register', urlencodedParser, auth_controller.user_register);
 // GET request for verifying user tokens 
-router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 // POST request for logging a user in
 router.post('/auth/login', urlencodedParser, auth_controller.user_login);
 // GET request for logging a user out
